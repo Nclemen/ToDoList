da8eb5e287e954f10f40a18a9c4a0756da92948c
Unknown <sonche17@hotmail.com>
2020-05-29 09:39:44 +0200

    created crud
    only need to change tasks so they have duration in minutes and a status (still in progress)

---
+++ inc/crud.inc.php
@@ -0,0 +1,51 @@
+<?php
+
+try {
+$dbh = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+  $values = array_map ( 'htmlspecialchars' , $_POST );
+  if ($values['action'] === 'addTask') {
+  $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  $sql = "INSERT INTO `Tasks`( `task_name`, `task_description`, `list_id`) VALUES ( :taskname, :taskdescription, :listid)";
+  $stmt = $dbh->prepare($sql);
+  $stmt->execute([":taskname"=>$values['taskName'],
+                 ":taskdescription"=>$values['taskDescription'],
+                 ":listid"=>$values['listId']]);
+} elseif ($values['action'] === 'addList'){
+  $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  $sql = "INSERT INTO Lists (listName)
+  VALUES (:listname)";
+  $stmt = $dbh->prepare($sql);
+  $stmt->execute([":listname"=>$values['listName']]);
+} elseif ($values['action'] === 'editTask') {
+  $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  $sql = "UPDATE `Tasks` SET `task_name`=:taskname,`task_description`=:description WHERE `id`= :id";
+  $stmt = $dbh->prepare($sql);
+  $stmt->execute([":taskname"=>$values['taskName'],
+                  ":id"=>$values['taskId'],
+                  ":description"=>$values['taskDescription']]);
+} elseif ($values['action'] === 'editList') {
+  echo 'editing list';
+  $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  $sql = "UPDATE `Lists` SET `listName`=:listname WHERE `id`= :id";
+  $stmt = $dbh->prepare($sql);
+  $stmt->execute([":listname"=>$values['listName'],
+                  ":id"=>$values['id']]);
+} elseif ($values['action'] === 'delete') {
+  $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  switch ($values['table']) {
+    case 'Tasks':
+      $sql = "DELETE FROM `Tasks` WHERE `id`=:id";
+      break;
+    case 'Lists':
+      $sql = "DELETE FROM `Lists` WHERE `id`=:id";
+      break;
+  }
+  $stmt = $dbh->prepare($sql);
+  $stmt->execute([":id"=>$values['id']]);
+}
+}
+}
+catch (PDOexception $e) {
+    echo "Error is: " . $e->getmessage();
+}
--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
  include 'inc/dbc.inc.php';
+ include 'inc/crud.inc.php';
 ?>
 
 <!DOCTYPE html>
@@ -8,9 +9,10 @@
     <meta charset="utf-8">
     <title></title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link href="//netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.css" rel="stylesheet">
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
   </head>
   <body>
     <div class="container">
@@ -21,15 +23,19 @@
     $sql = "SELECT * FROM Lists";
     $lists = $dbh->query($sql);
     foreach ($lists as $list) {
+      $listidentifier = str_replace( " " , "" ,$list['listName'] . $list['id']);
 ?>
 <div class="card" style="width: 18rem;">
   <div class="card-body">
-    <h5 class="card-title"><?php echo $list['listName'] ?></h5>
+    <h5 class="card-title"><?php echo $list['listName'] ?>
+      <button type="button" class="btn btn-info" data-toggle="modal" data-target=".edit-list-modal-<?php echo $listidentifier ?>"><i class="fas icon-pencil"></i></button>
+      <button type="button" class="btn btn-danger" data-toggle="modal" data-target=".delete-item-modal-<?php echo $listidentifier ?>"><i class="fas icon-trash"></i></button>
+    </h5>
 <?php
-      $sql2 = "SELECT * FROM Tasks WHERE list_id = " . $list['id'];
-      $tasks = $dbh->query($sql2);
+      $sql = "SELECT * FROM Tasks WHERE list_id = " . $list['id'];
+      $tasks = $dbh->query($sql);
       foreach ($tasks as $task) {
-        $taskidentifier = $task['task_name'] . $task['id'];
+        $taskidentifier = str_replace( " " , "" ,$task['task_name'] . $task['id']);
         ?>
         <div id="accordion<?php echo $task['id'] ?>">
         <div class="card">
@@ -43,15 +49,106 @@
         <div id="collapse<?php echo $taskidentifier ?>" class="collapse " aria-labelledby="heading<?php echo $taskidentifier ?>" data-parent="#accordion">
           <div class="card-body">
             <?php echo $task['task_description'] ?>
+            <div>
+              <button type="button" class="btn btn-info" data-toggle="modal" data-target=".edit-task-<?php echo $taskidentifier ?>">edit task</button>
+              <button type="button" class="btn btn-danger" data-toggle="modal" data-target=".delete-item-modal-<?php echo $taskidentifier ?>">delete task</button>
+            </div>
+          </div>
+        </div>
+        </div>
+        </div>
+        <div class="modal fade edit-task-<?php echo $taskidentifier ?>" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-lg">
+          <div class="modal-content">
+            <form action="" name="taskDescription" method="post">
+                <input type="hidden" name="taskId" value="<?php echo $task['id'] ?>">
+                <input type="hidden" name="action" value="editTask">
+                <div class="form-group">
+                 <label for="taskName">task name</label>
+                 <input type="text" name="taskName" value="<?php echo $task['task_name'] ?>">
+                </div>
+                <div class="form-group">
+                  <label for="taskDescription">Example textarea</label>
+                  <textarea class="form-control" name="taskDescription" id="taskDescription" rows="3" maxlength="255"><?php echo $task['task_description'] ?></textarea>
+                </div>
+                <input type="submit">
+            </form>
+          </div>
+        </div>
+        </div>
+        <div class="modal fade delete-item-modal-<?php echo $taskidentifier ?>" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+          <div class="modal-dialog modal-lg">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h5 class="modal-title">Are you sure you want to delete this task?</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+              <div class="modal-body">
+              <form action="" method="post">
+                  <input type="hidden" name="id" value="<?php echo $task['id'] ?>">
+                  <input type="hidden" name="table" value="Tasks">
+                  <input type="submit" name="action" value="delete">
+              </form>
+            </div>
+            </div>
           </div>
         </div>
         <?php
-        ?>
-</div>
-</div>
-        <?php
       };
       ?>
+      <div class="card">
+      <div class="card-header" id="">
+        <h5 class="mb-0">
+          <button type="button" class="btn btn-primary" data-toggle="modal" data-target=".new-task-<?php echo $list['id'] ?>">add task</button>
+        </h5>
+      </div>
+      </div>
+      <div class="modal fade new-task-<?php echo $list['id'] ?>" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+      <div class="modal-dialog modal-lg">
+        <div class="modal-content">
+          <form action="" name="taskDescription" method="post">
+              <input type="hidden" name="listId" value="<?php echo $list['id'] ?>">
+              <input type="hidden" name="action" value="addTask">
+              <div class="form-group">
+               <label for="taskName">task name</label>
+               <input type="text" name="taskName">
+              </div>
+              <div class="form-group">
+                <label for="taskDescription">Example textarea</label>
+                <textarea class="form-control" name="taskDescription" id="taskDescription" rows="3" maxlength="255"></textarea>
+              </div>
+              <input type="submit">
+          </form>
+        </div>
+      </div>
+    </div>
+    </div>
+  </div>
+  <div class="modal fade edit-list-modal-<?php echo $listidentifier ?>" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-lg">
+      <div class="modal-content">
+        <form action="" method="post">
+            <input type="hidden" name="action" value="editList">
+            <input type="hidden" name="id" value="<?php echo $list['id'] ?>">
+            <label for="listName">list name</label>
+            <input type="text" name="listName" value="<?php echo $list['listName'] ?>">
+            <input type="submit">
+        </form>
+      </div>
+    </div>
+  </div>
+  <div class="modal fade delete-item-modal-<?php echo $listidentifier ?>" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-lg">
+      <div class="modal-content">
+        <form action="" method="post">
+            <input type="hidden" name="action" value="delete">
+            <input type="hidden" name="id" value="<?php echo $list['id'] ?>">
+            <input type="hidden" name="table" value="Lists">
+            <input class="btn btn-danger" type="submit" value="delete">
+        </form>
+      </div>
     </div>
   </div>
       <?php
@@ -59,9 +156,23 @@
     $dbh = null;
     }
     catch (PDOexception $e) {
-        echo "Error is: " . $e-> etmessage();
+        echo "Error is: " . $e->getmessage();
     }
     ?>
+    <button type="button" class="btn btn-primary" data-toggle="modal" data-target=".bd-new-list-modal">add new list</button>
+
+    <div class="modal fade bd-new-list-modal" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+      <div class="modal-dialog modal-lg">
+        <div class="modal-content">
+          <form action="" method="post">
+              <input type="hidden" name="action" value="addList">
+              <label for="listName">list name</label>
+              <input type="text" name="listName">
+              <input type="submit">
+          </form>
+        </div>
+      </div>
+    </div>
   </div>
   </div>
 
